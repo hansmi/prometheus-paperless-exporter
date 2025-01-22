9a59d5a0e8062aa37a1ab3313496916019706eef
Michael Hanselmann <public@hansmi.ch>
2025-01-22 23:09:40 +0100

    Test item counts via full collector

--- collector_test.go
+++ collector_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"mime"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -12,9 +13,27 @@ import (
 )
 
 func TestCollector(t *testing.T) {
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	contentTypeJson := mime.FormatMediaType("application/json", nil)
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.NotFoundHandler())
+	mux.HandleFunc("/api/", func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "{}")
-	}))
+	})
+	mux.HandleFunc("/api/groups/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", contentTypeJson)
+		io.WriteString(w, `{"count": 10}`)
+	})
+	mux.HandleFunc("/api/users/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", contentTypeJson)
+		io.WriteString(w, `{"count": 20}`)
+	})
+	mux.HandleFunc("/api/documents/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", contentTypeJson)
+		io.WriteString(w, `{"count": 30}`)
+	})
+
+	ts := httptest.NewServer(mux)
 	t.Cleanup(ts.Close)
 
 	cl := client.New(client.Options{
@@ -27,5 +46,14 @@ func TestCollector(t *testing.T) {
 # HELP paperless_task_status_info Task status names.
 # TYPE paperless_task_status_info gauge
 paperless_task_status_info{status="success"} 1
+# HELP paperless_groups Number of user groups.
+# TYPE paperless_groups gauge
+paperless_groups 10
+# HELP paperless_users Number of users.
+# TYPE paperless_users gauge
+paperless_users 20
+# HELP paperless_documents Number of documents.
+# TYPE paperless_documents gauge
+paperless_documents 30
 `)
 }
