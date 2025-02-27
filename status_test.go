988f791f043b78493472825ae0b47ee607e59b85
Andy Grunwald <andygrunwald@gmail.com>
2025-02-27 21:48:44 +0100

    Rename Prometheus metrics according to their naming scheme
    See https://prometheus.io/docs/practices/naming/

--- status_test.go
+++ status_test.go
@@ -99,9 +99,9 @@ func TestStatusCollect(t *testing.T) {
 # HELP paperless_status_celery_status Status of celery. 1 is OK, 0 is not OK.
 # TYPE paperless_status_celery_status gauge
 paperless_status_celery_status 1
-# HELP paperless_status_classifier_last_trained Seconds since the last time the classifier has been trained.
-# TYPE paperless_status_classifier_last_trained gauge
-paperless_status_classifier_last_trained 5
+# HELP paperless_status_classifier_last_trained_seconds Seconds since the last time the classifier has been trained.
+# TYPE paperless_status_classifier_last_trained_seconds gauge
+paperless_status_classifier_last_trained_seconds 5
 # HELP paperless_status_classifier_status Status of the classifier. 1 is OK, 0 is not OK.
 # TYPE paperless_status_classifier_status gauge
 paperless_status_classifier_status 1
@@ -111,20 +111,20 @@ paperless_status_database_status 1
 # HELP paperless_status_database_unapplied_migrations Number of unapplied database migrations.
 # TYPE paperless_status_database_unapplied_migrations gauge
 paperless_status_database_unapplied_migrations 0
-# HELP paperless_status_index_last_modified Seconds since the last time the index has been modified.
-# TYPE paperless_status_index_last_modified gauge
-paperless_status_index_last_modified 5
+# HELP paperless_status_index_last_modified_seconds Seconds since the last time the index has been modified.
+# TYPE paperless_status_index_last_modified_seconds gauge
+paperless_status_index_last_modified_seconds 5
 # HELP paperless_status_index_status Status of the index. 1 is OK, 0 is not OK.
 # TYPE paperless_status_index_status gauge
 paperless_status_index_status 1
 # HELP paperless_status_redis_status Status of redis. 1 is OK, 0 is not OK.
 # TYPE paperless_status_redis_status gauge
 paperless_status_redis_status 1
-# HELP paperless_status_storage_available Available storage of Paperless in bytes.
-# TYPE paperless_status_storage_available gauge
-paperless_status_storage_available 1.3406437376e+10
-# HELP paperless_status_storage_total Total storage of Paperless in bytes.
-# TYPE paperless_status_storage_total gauge
-paperless_status_storage_total 2.147483648e+10
+# HELP paperless_status_storage_available_bytes Available storage of Paperless in bytes.
+# TYPE paperless_status_storage_available_bytes gauge
+paperless_status_storage_available_bytes 1.3406437376e+10
+# HELP paperless_status_storage_total_bytes Total storage of Paperless in bytes.
+# TYPE paperless_status_storage_total_bytes gauge
+paperless_status_storage_total_bytes 2.147483648e+10
 `)
 }
