6be27ff89adf5b1c31978c193054dadf4f6e1c75
Michael Hanselmann <public@hansmi.ch>
2025-03-05 23:27:44 +0100

    Construct collectors before the multiCollector
    No functional change, just style.

--- collector.go
+++ collector.go
@@ -8,26 +8,26 @@ import (
 )
 
 func newCollector(cl *client.Client, timeout time.Duration, enableRemoteNetwork bool) prometheus.Collector {
-	multiCollector := &multiCollector{
-		timeout: timeout,
-		members: []multiCollectorMember{
-			newTagCollector(cl),
-			newCorrespondentCollector(cl),
-			newDocumentTypeCollector(cl),
-			newStoragePathCollector(cl),
-			newTaskCollector(cl),
-			newLogCollector(cl),
-			newGroupCollector(cl),
-			newUserCollector(cl),
-			newDocumentCollector(cl),
-			newStatusCollector(cl),
-			newStatisticsCollector(cl),
-		},
+	members := []multiCollectorMember{
+		newTagCollector(cl),
+		newCorrespondentCollector(cl),
+		newDocumentTypeCollector(cl),
+		newStoragePathCollector(cl),
+		newTaskCollector(cl),
+		newLogCollector(cl),
+		newGroupCollector(cl),
+		newUserCollector(cl),
+		newDocumentCollector(cl),
+		newStatusCollector(cl),
+		newStatisticsCollector(cl),
 	}
 
 	if enableRemoteNetwork {
-		multiCollector.members = append(multiCollector.members, newRemoteVersionCollector(cl))
+		members = append(members, newRemoteVersionCollector(cl))
 	}
 
-	return multiCollector
+	return &multiCollector{
+		timeout: timeout,
+		members: members,
+	}
 }
